7d4396cc49a06716e39f0a26d4383fe4ac35fba0
3TimesDavid <davidmarpal@gmail.com>
2025-09-16 21:55:10 +0200

    Add files via upload

---
+++ about_me.py
@@ -0,0 +1,7 @@
+name = "Ana"
+age = 28
+city = "Valencia"
+
+print(f"Hi! My name is {name}, I am {age} years old and I live in {city}.")
+
+print("Hi! My name is", name, "I am", age, "years old and I live in", city,".")
---
+++ day1.py
@@ -0,0 +1,9 @@
+name = "Tu nombre"
+age = 25
+city = "Tu ciudad"
+
+
+
+print("My name is", name)
+print("I am", age, "years ond")
+print("I live in", city)
