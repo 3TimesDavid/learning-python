0dcc1cfae37c74d184cc0a005bb3166e82d6e67a
3TimesDavid <davidmarpal@gmail.com>
2025-09-20 17:10:14 +0200

    Delete about_me.py

--- about_me.py
+++
@@ -1,7 +0,0 @@
-name = "Ana"
-age = 28
-city = "Valencia"
-
-print(f"Hi! My name is {name}, I am {age} years old and I live in {city}.")
-
-print("Hi! My name is", name, "I am", age, "years old and I live in", city,".")
